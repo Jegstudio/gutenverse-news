5ccc53c86d047b771109a66916a90df5e47438a9
Fauzan Edris <fauzanedris@yahoo.com>
2025-05-20 17:59:26 +0800

    [fix/fix-bug] fix: wrong regex, due to this commit 690e1d869c1f39f1e1715e3b4ba12ae25ab0d9d4

--- gutenverse-news/include/class/style/class-styleabstract.php
+++ gutenverse-news/include/class/style/class-styleabstract.php
@@ -80,7 +80,7 @@ abstract class StyleAbstract extends StyleInterface {
 			$name = $this->name;
 		}
 		if ( $name ) {
-			preg_match( '/^block|hero/', 'block-1', $matches );
+			preg_match( '/^block|hero/', $name, $matches );
 
 			if ( ! empty( $matches ) && in_array( $matches[0], array( 'block', 'hero' ) ) ) {
 				$name = preg_replace( '/-(\d)$/', '-0$1', $name );
