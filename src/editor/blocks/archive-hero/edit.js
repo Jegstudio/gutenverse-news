df19378a76dc90da07c355c662a68e6bd678e8fc
Fauzan Edris <fauzanedris@yahoo.com>
2025-05-23 15:09:46 +0800

    [dev/migrate-core] migrate to new core for archive hero block

--- src/editor/blocks/archive-hero/edit.js
+++ src/editor/blocks/archive-hero/edit.js
@@ -1,22 +1,26 @@
 import { compose } from '@wordpress/compose';
-import { withCustomStyle } from 'gutenverse-core/hoc';
+import { withPartialRender, withPassRef } from 'gutenverse-core/hoc';
 import { useBlockProps } from '@wordpress/block-editor';
 import classnames from 'classnames';
-import { PanelController } from 'gutenverse-core/controls';
+import { BlockPanelController } from 'gutenverse-core/controls';
 import { panelList } from './panels/panel-list';
 import { useAnimationEditor } from 'gutenverse-core/hooks';
 import { useDisplayEditor } from 'gutenverse-core/hooks';
 import { useEffect, useRef } from '@wordpress/element';
 import { HeroHandler } from '../../part/hero';
-import { withCopyElementToolbar } from 'gutenverse-core/hoc';
+import { useDynamicStyle, useGenerateElementId } from 'gutenverse-core/styling';
+import { CopyElementToolbar } from 'gutenverse-core/components';
+import getBlockStyle from './styles/block-style';
+// import getBlockStyle from '../../control-panel/hero-style';
 
 const ArchiveHero = compose(
-    withCustomStyle(panelList),
-    withCopyElementToolbar()
+    withPartialRender,
+    withPassRef
 )((props) => {
     const {
         attributes,
-        setElementRef,
+        clientId,
+        setBlockRef,
         isSelected
     } = props;
 
@@ -34,23 +38,27 @@ const ArchiveHero = compose(
     const animationClass = useAnimationEditor(attributes);
     const displayClass = useDisplayEditor(attributes);
 
-    const heroSliderRef = useRef();
+    const elementRef = useRef(null);
+
+    useGenerateElementId(clientId, elementId, elementRef);
+    useDynamicStyle(elementId, attributes, getBlockStyle, elementRef);
 
     useEffect(() => {
-        if (heroSliderRef.current) {
-            setElementRef(heroSliderRef.current);
+        if (elementRef) {
+            setBlockRef(elementRef);
         }
-    }, [heroSliderRef.current]);
+    }, [elementRef]);
 
     const blockProps = useBlockProps({
         className: classnames('gvnews-block',
             'gvnews-block-wrapper', 'gvnews-archive-hero', elementId, animationClass, displayClass),
-        ref: heroSliderRef
+        ref: elementRef
     });
 
     return (
         <>
-            <PanelController panelList={panelList} {...props} />
+            <CopyElementToolbar {...props} />
+            <BlockPanelController panelList={panelList} props={props} elementRef={elementRef} />
             <div {...blockProps}>
                 <div className="guten-raw-wrapper gvnews-editor">
                     <div className="gvnews-element-overlay" style={{ pointerEvents: isSelected ? 'none' : 'auto' }}></div>
@@ -58,7 +66,7 @@ const ArchiveHero = compose(
                         {...{
                             type: 'archive',
                             heroType: heroType,
-                            heroSliderRef,
+                            elementRef,
                             columnWidth,
                             sliderItem: 2,
                             postOffset: 0,
---
+++ src/editor/blocks/archive-hero/styles/block-style.js
@@ -0,0 +1,286 @@
+import { isNotEmpty } from 'gutenverse-core/helper';
+
+const getPanelHeroStyle = (elementId, attributes) => {
+    let data = [];
+
+    for (let i = 1; i <= 7; i++) {
+        isNotEmpty(attributes[`heroItem${i}Enable`]) && isNotEmpty(attributes[`heroItem${i}Background`]) && data.push({
+            'type': 'background',
+            'id': `heroItem${i}Background`,
+            'selector': `.gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_hero_item_${i} .gvnews_thumb a > div:${i === 5 ? 'after' : 'before'}`,
+        });
+    }
+
+    return data;
+};
+
+const getBlockStyle = (elementId, attributes) => {
+    let data = [...getPanelHeroStyle(elementId, attributes)];
+
+    /**
+     * Panel Design
+     */
+    isNotEmpty(attributes['titleTypography']) && data.push({
+        'type': 'typography',
+        'id': 'titleTypography',
+        'selector': `.gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_post_title > a`,
+    });
+
+    isNotEmpty(attributes['metaTypography']) && data.push({
+        'type': 'typography',
+        'id': 'metaTypography',
+        'selector': `.gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_post_meta, .gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_post_meta .fa, .gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_heroblock .gvnews_subcat_list > li > a:hover, .gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_pl_md_card .gvnews_post_category a, .gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_heroblock .gvnews_subcat_list > li > a.current, .gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_pl_md_5 .gvnews_post_meta, .gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_pl_md_5 .gvnews_post_meta .fa, .gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_post_category a`,
+    });
+
+    /**
+     * Panel Background
+     */
+    isNotEmpty(attributes['background']) && data.push({
+        'type': 'background',
+        'id': 'background',
+        'selector': `.${elementId} .gvnews_heroblock`,
+    });
+
+    isNotEmpty(attributes['backgroundHover']) && data.push({
+        'type': 'background',
+        'id': 'backgroundHover',
+        'selector': `.${elementId} .gvnews_heroblock:hover`,
+    });
+
+    /**
+     * Panel Border
+     */
+    isNotEmpty(attributes['border']) && data.push({
+        'type': 'border',
+        'id': 'border',
+        'selector': `.${elementId} .gvnews_heroblock`,
+    });
+
+    isNotEmpty(attributes['borderResponsive']) && data.push({
+        'type': 'borderResponsive',
+        'id': 'borderResponsive',
+        'selector': `.${elementId} .gvnews_heroblock`,
+    });
+
+    isNotEmpty(attributes['borderHover']) && data.push({
+        'type': 'border',
+        'id': 'borderHover',
+        'selector': `.${elementId} .gvnews_heroblock:hover`,
+    });
+
+    isNotEmpty(attributes['borderHoverResponsive']) && data.push({
+        'type': 'borderResponsive',
+        'id': 'borderHoverResponsive',
+        'selector': `.${elementId} .gvnews_heroblock:hover`,
+    });
+
+    isNotEmpty(attributes['boxShadow']) && data.push({
+        'type': 'boxShadow',
+        'id': 'boxShadow',
+        'selector': `.${elementId} .gvnews_heroblock`,
+        'properties': [
+            {
+                'name': 'box-shadow',
+                'valueType': 'direct'
+            }
+        ],
+    });
+
+    isNotEmpty(attributes['boxShadowHover']) && data.push({
+        'type': 'boxShadow',
+        'id': 'boxShadowHover',
+        'selector': `.${elementId} .gvnews_heroblock:hover`,
+        'properties': [
+            {
+                'name': 'box-shadow',
+                'valueType': 'direct'
+            }
+        ],
+    });
+
+    /**
+     * Panel Positioning
+     */
+    isNotEmpty(attributes['positioningType']) && data.push(
+        {
+            'type': 'positioning',
+            'id': 'positioningType',
+            'selector': `.${elementId} .gvnews_heroblock`,
+            'skipDeviceType': 'first',
+            'attributeType': 'type',
+            'multiAttr': {
+                'positioningType': attributes['positioningType'],
+                'inBlock': attributes['inBlock']
+            }
+        },
+    );
+
+    isNotEmpty(attributes['positioningType']) && isNotEmpty(attributes['positioningWidth']) && data.push(
+        {
+            'type': 'positioning',
+            'id': 'positioningType',
+            'selector': `.${elementId} .gvnews_heroblock`,
+            'skipDeviceType': 'second',
+            'attributeType': 'type',
+            'multiAttr': {
+                'positioningWidth': attributes['positioningWidth'],
+                'positioningType': attributes['positioningType'],
+                'inBlock': attributes['inBlock']
+            }
+        }
+    );
+
+    isNotEmpty(attributes['positioningWidth']) && isNotEmpty(attributes['positioningType']) && data.push({
+        'type': 'positioning',
+        'id': 'positioningWidth',
+        'selector': `.${elementId} .gvnews_heroblock`,
+        'skipDeviceType': 'first',
+        'attributeType': 'width',
+        'multiAttr': {
+            'positioningWidth': attributes['positioningWidth'],
+            'positioningType': attributes['positioningType'],
+            'inBlock': attributes['inBlock']
+        }
+    });
+
+    isNotEmpty(attributes['positioningAlign']) && data.push(
+        {
+            'type': 'plain',
+            'id': 'positioningAlign',
+            'responsive': true,
+            'properties': [
+                {
+                    'name': 'align-self',
+                    'valueType': 'direct'
+                }
+            ],
+            'selector': `.${elementId} .gvnews_heroblock`,
+        },
+        {
+            'type': 'positioning',
+            'id': 'positioningAlign',
+            'properties': [
+                {
+                    'name': 'vertical-align',
+                    'valueType': 'direct'
+                }
+            ],
+            'attributeType': 'align',
+            'selector': `.${elementId} .gvnews_heroblock`,
+        }
+    );
+
+    isNotEmpty(attributes['positioningLocation']) && attributes['positioningLocation'] !== 'default' && data.push({
+        'type': 'plain',
+        'id': 'positioningLocation',
+        'properties': [
+            {
+                'name': 'position',
+                'valueType': 'direct'
+            }
+        ],
+        'selector': `.${elementId} .gvnews_heroblock`,
+    });
+
+    isNotEmpty(attributes['positioningLeft']) && isNotEmpty(attributes['positioningLocation']) && attributes['positioningLocation'] !== 'default' && data.push({
+        'type': 'positioning',
+        'id': 'positioningLeft',
+        'properties': [
+            {
+                'name': 'left',
+                'valueType': 'direct'
+            }
+        ],
+        'responsive': true,
+        'selector': `.${elementId} .gvnews_heroblock`,
+        'attributeType': 'custom',
+    });
+
+    isNotEmpty(attributes['positioningRight']) && isNotEmpty(attributes['positioningLocation']) && attributes['positioningLocation'] !== 'default' && data.push({
+        'type': 'positioning',
+        'id': 'positioningRight',
+        'properties': [
+            {
+                'name': 'right',
+                'valueType': 'direct'
+            }
+        ],
+        'responsive': true,
+        'selector': `.${elementId} .gvnews_heroblock`,
+        'attributeType': 'custom',
+    });
+
+    isNotEmpty(attributes['positioningTop']) && isNotEmpty(attributes['positioningLocation']) && attributes['positioningLocation'] !== 'default' && data.push({
+        'type': 'positioning',
+        'id': 'positioningTop',
+        'properties': [
+            {
+                'name': 'top',
+                'valueType': 'direct'
+            }
+        ],
+        'responsive': true,
+        'selector': `.${elementId} .gvnews_heroblock`,
+        'attributeType': 'custom',
+    });
+
+    isNotEmpty(attributes['positioningBottom']) && isNotEmpty(attributes['positioningLocation']) && attributes['positioningLocation'] !== 'default' && data.push({
+        'type': 'positioning',
+        'id': 'positioningBottom',
+        'properties': [
+            {
+                'name': 'bottom',
+                'valueType': 'direct'
+            }
+        ],
+        'responsive': true,
+        'selector': `.${elementId} .gvnews_heroblock`,
+        'attributeType': 'custom',
+    });
+
+    /**
+     * Panel Spacing
+     */
+    isNotEmpty(attributes['margin']) && data.push({
+        'type': 'dimension',
+        'id': 'margin',
+        'responsive': true,
+        'properties': [
+            {
+                'name': 'margin',
+                'valueType': 'direct'
+            }
+        ],
+        'selector': `.${elementId} .gvnews_heroblock`,
+    });
+
+    isNotEmpty(attributes['padding']) && data.push({
+        'type': 'dimension',
+        'id': 'padding',
+        'responsive': true,
+        'properties': [
+            {
+                'name': 'padding',
+                'valueType': 'direct'
+            }
+        ],
+        'selector': `.${elementId} .gvnews_heroblock`,
+    });
+
+    isNotEmpty(attributes['zIndex']) && data.push({
+        'type': 'plain',
+        'id': 'zIndex',
+        'responsive': true,
+        'properties': [
+            {
+                'name': 'z-index',
+                'valueType': 'direct'
+            }
+        ],
+        'selector': `.${elementId} .gvnews_heroblock`,
+    });
+
+    return data;
+};
+
+export default getBlockStyle;
