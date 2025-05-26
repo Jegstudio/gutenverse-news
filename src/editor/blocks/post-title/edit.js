6127f903fb1f3d08cb35014b42d6ecd597adbdd4
Fauzan Edris <fauzanedris@yahoo.com>
2025-05-26 14:39:52 +0800

    [dev/migrate-core] migrate to new core for post title block

--- src/editor/blocks/post-title/edit.js
+++ src/editor/blocks/post-title/edit.js
@@ -1,22 +1,25 @@
 import { compose } from '@wordpress/compose';
-import { withCustomStyle } from 'gutenverse-core/hoc';
+import { withPartialRender, withPassRef } from 'gutenverse-core/hoc';
 import { useBlockProps } from '@wordpress/block-editor';
 import classnames from 'classnames';
-import { PanelController } from 'gutenverse-core/controls';
+import { BlockPanelController } from 'gutenverse-core/controls';
 import { panelList } from './panels/panel-list';
 import { useAnimationEditor } from 'gutenverse-core/hooks';
 import { useDisplayEditor } from 'gutenverse-core/hooks';
 import QueryTitle from '../../query/query-title';
 import { useRef, useEffect } from '@wordpress/element';
-import { withCopyElementToolbar } from 'gutenverse-core/hoc';
+import { useDynamicStyle, useGenerateElementId } from 'gutenverse-core/styling';
+import { CopyElementToolbar } from 'gutenverse-core/components';
+import getBlockStyle from './styles/block-style';
 
 const PostTitle = compose(
-    withCustomStyle(panelList),
-    withCopyElementToolbar()
+    withPartialRender,
+    withPassRef
 )((props) => {
     const {
         attributes,
-        setElementRef
+        clientId,
+        setBlockRef
     } = props;
 
     const {
@@ -25,13 +28,17 @@ const PostTitle = compose(
 
     const animationClass = useAnimationEditor(attributes);
     const displayClass = useDisplayEditor(attributes);
-    const titleRichTextRef = useRef();
+
+    const elementRef = useRef(null);
+
+    useGenerateElementId(clientId, elementId, elementRef);
+    useDynamicStyle(elementId, attributes, getBlockStyle, elementRef);
 
     useEffect(() => {
-        if (titleRichTextRef.current) {
-            setElementRef(titleRichTextRef.current);
+        if (elementRef) {
+            setBlockRef(elementRef);
         }
-    }, [titleRichTextRef]);
+    }, [elementRef]);
 
     const blockProps = useBlockProps({
         className: classnames(
@@ -42,15 +49,17 @@ const PostTitle = compose(
             animationClass,
             displayClass,
         ),
+        ref: elementRef
     });
 
     return <>
-        <PanelController panelList={panelList} {...props} />
+        <CopyElementToolbar {...props} />
+        <BlockPanelController panelList={panelList} props={props} elementRef={elementRef} />
         <QueryTitle {
             ...{
                 blockProps,
                 type: 'post',
-                titleRef: titleRichTextRef
+                titleRef: elementRef
             }
         } />
     </>;
