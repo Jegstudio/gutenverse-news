f0c6c5992332fbe46032304485dc7d583e81e4eb
Fauzan Edris <fauzanedris@yahoo.com>
2025-05-26 16:48:08 +0800

    [dev/migrate-core] migrate to new core for header block

--- src/editor/blocks/header/edit.js
+++ src/editor/blocks/header/edit.js
@@ -1,10 +1,10 @@
 import { compose } from '@wordpress/compose';
-import {  useState, useEffect, Fragment }  from '@wordpress/element';
-import { withCustomStyle } from 'gutenverse-core/hoc';
+import { useState, useEffect, Fragment } from '@wordpress/element';
+import { withPartialRender, withPassRef } from 'gutenverse-core/hoc';
 import { useBlockProps } from '@wordpress/block-editor';
 import classnames from 'classnames';
 import { __ } from '@wordpress/i18n';
-import { PanelController } from 'gutenverse-core/controls';
+import { BlockPanelController } from 'gutenverse-core/controls';
 import { panelList } from './panels/panel-list';
 import { useAnimationEditor } from 'gutenverse-core/hooks';
 import { useDisplayEditor } from 'gutenverse-core/hooks';
@@ -13,16 +13,19 @@ import { addQueryArgs } from '@wordpress/url';
 import { RawHTML } from '@wordpress/element';
 import { ModuleSkeleton } from '../../part/placeholder';
 import HeaderModule from '../../part/header';
-import { withCopyElementToolbar } from 'gutenverse-core/hoc';
 import { useRef } from '@wordpress/element';
+import { useDynamicStyle, useGenerateElementId } from 'gutenverse-core/styling';
+import { CopyElementToolbar } from 'gutenverse-core/components';
+import getBlockStyle from './styles/block-style';
 
 const HeaderBlock = compose(
-    withCustomStyle(panelList),
-    withCopyElementToolbar()
+    withPartialRender,
+    withPassRef
 )((props) => {
     const {
         attributes,
-        setElementRef,
+        clientId,
+        setBlockRef
     } = props;
 
     const {
@@ -33,13 +36,16 @@ const HeaderBlock = compose(
         headerType,
     } = attributes;
 
-    const blockStyleRef = useRef();
+    const elementRef = useRef(null);
+
+    useGenerateElementId(clientId, elementId, elementRef);
+    useDynamicStyle(elementId, attributes, getBlockStyle, elementRef);
 
     useEffect(() => {
-        if (blockStyleRef.current) {
-            setElementRef(blockStyleRef.current);
+        if (elementRef) {
+            setBlockRef(elementRef);
         }
-    }, [blockStyleRef]);
+    }, [elementRef]);
 
     const animationClass = useAnimationEditor(attributes);
     const displayClass = useDisplayEditor(attributes);
@@ -60,11 +66,12 @@ const HeaderBlock = compose(
             animationClass,
             displayClass,
         ),
-        ref: blockStyleRef
+        ref: elementRef
     });
 
     return <>
-        <PanelController panelList={panelList} {...props} />
+        <CopyElementToolbar {...props} />
+        <BlockPanelController panelList={panelList} props={props} elementRef={elementRef} />
         <div  {...blockProps}>
             <div className="gvnews-raw-wrapper gvnews-editor">
                 <HeaderModule {...headerData} />
