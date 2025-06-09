96d735b85f8dc8ee4076aadf580187d6f6b0c41e
Fauzan Edris <fauzanedris@yahoo.com>
2025-06-09 17:37:52 +0800

    [dev/migrate-core] fix: get thumbnail in rss block

--- gutenverse-news/include/class/util/class-feed.php
+++ gutenverse-news/include/class/util/class-feed.php
@@ -16,12 +16,81 @@ namespace GUTENVERSE\NEWS\Util;
  * @author Jegstudio
  */
 class Feed {
+	/**
+	 * RSS Feed atribute
+	 *
+	 * @var array
+	 */
+	public $attr;
+
+	/**
+	 * RSS Feed Post ID
+	 *
+	 * @var string
+	 */
+	public $ID;
+
+	/**
+	 * RSS Feed Title
+	 *
+	 * @var string
+	 */
+	public $title;
+
+	/**
+	 * RSS Feed Permalink
+	 *
+	 * @var string
+	 */
+	public $permalink;
+
+	/**
+	 * RSS Feed Description
+	 *
+	 * @var string
+	 */
+	public $description;
+
+	/**
+	 * RSS Feed Post Author Name
+	 *
+	 * @var string
+	 */
+	public $post_author_name;
+
+	/**
+	 * RSS Feed Post Published Date
+	 *
+	 * @var integer
+	 */
+	public $publish_date;
+
+	/**
+	 * RSS Feed Post Update Date
+	 *
+	 * @var integer||null
+	 */
+	public $update_date;
+
+	/**
+	 * RSS Feed Post Update Date
+	 *
+	 * @var integer
+	 */
+	public $featured;
+
+	/**
+	 * RSS Feed Filter
+	 *
+	 * @var string
+	 */
+	public $filter;
 
 	/**
 	 * Method __construct
 	 *
-	 * @param object $feed_object feed object.
-	 * @param array  $attr        attributes.
+	 * @param \SimplePie\Item|object $feed_object feed object.
+	 * @param array                  $attr        attributes.
 	 *
 	 * @return void
 	 */
@@ -34,7 +103,7 @@ class Feed {
 		$this->post_author_name = isset( $feed_object->get_author()->name ) ? $feed_object->get_author()->name : '';
 		$this->publish_date     = $feed_object->get_date( 'U' );
 		$this->update_date      = $feed_object->get_updated_date( 'U' );
-		$this->featured         = isset( $attr['thumbnail'] ) ? $this->thumbnail( $feed_object->get_thumbnail() ) : '';
+		$this->featured         = $attr['thumbnail'] ? $this->thumbnail( $feed_object ) : '';
 	}
 
 	/**
@@ -52,14 +121,25 @@ class Feed {
 	/**
 	 * Method thumbnail
 	 *
-	 * @param string $image image.
+	 * @param \SimplePie\Item|object $feed_object Feed Object.
 	 *
-	 * @return string
+	 * @return html|string
 	 */
-	private function thumbnail( $image ) {
+	private function thumbnail( $feed_object ) {
+		$image_thumnail = $feed_object->get_thumbnail();
 
-		if ( is_array( $image ) ) {
-			$image = $image['url'];
+		if ( is_array( $image_thumnail ) ) {
+			$image = $image_thumnail['url'];
+		} else {
+			$enclosure = $feed_object->get_enclosure();
+
+			if ( is_object( $enclosure ) && ! empty( $enclosure->link ) && $this->is_image_link( $enclosure ) ) {
+				$image = $enclosure->link;
+			} else {
+				$first_image = $this->get_first_image_url( $feed_object->get_content() );
+
+				$image = ! empty( $first_image ) ? $first_image : false;
+			}
 		}
 
 		return $image ? '<img src="' . $image . '">' : '';
@@ -85,5 +165,51 @@ class Feed {
 		}
 
 		return $this->featured;
+	}
+
+	/**
+	 * Get first image from RSS feed content if the content not provide the post thumbnial from enclosure tag.
+	 *
+	 * @since x.x.x
+	 *
+	 * @param html|string $html an string that contain post content.
+	 */
+	private function get_first_image_url( $html ) {
+		if ( ! empty( $html ) && preg_match( '/<img.+?src="(.+?)"/', $html, $matches ) ) {
+			return $matches[1];
+		}
+
+		return false;
+	}
+
+	/**
+	 * Check if enclosure link type is image
+	 *
+	 * @since x.x.x
+	 *
+	 * @param object $enclosure enclusure content from RSS Feeds items.
+	 */
+	private function is_image_link( $enclosure ) {
+		if ( isset( $enclosure->type ) && strpos( $enclosure->type, 'image/' ) === 0 ) {
+			return true;
+		}
+
+		$img_extension = array( 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'svg' );
+		$path          = wp_parse_url( $enclosure->link, PHP_URL_PATH );
+		$extension     = strtolower( pathinfo( $path, PATHINFO_EXTENSION ) );
+
+		if ( isset( $extension ) && in_array( $extension, $img_extension ) ) {
+			return true;
+		} else {
+			$headers = get_headers( $enclosure->link, 1 );
+
+			if ( isset( $headers['Content-Type'] ) ) {
+				if ( strpos( $headers['Content-Type'], 'image/' ) === 0 ) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 }
