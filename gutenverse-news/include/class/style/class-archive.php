74221658afc5fa72eaf12375ae7a29fb4e715a73
Hakim <ragamgit78@gmail.com>
2025-06-13 15:01:16 +0800

    [fix/archive] fix inject_style 2x in hero margin

--- gutenverse-news/include/class/style/class-archive.php
+++ gutenverse-news/include/class/style/class-archive.php
@@ -268,27 +268,23 @@ class Archive extends StyleAbstract {
 		if ( 'gutenverse/news-archive-hero' === $this->name ) {
 			if ( isset( $this->attrs['heroMargin'] ) ) {
 				$this->inject_style(
-					$this->inject_style(
-						array(
-							'selector'       => ".gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_heroblock_wrapper",
-							'property'       => function ( $value ) {
-								return "margin: 0 0 -{$value}px -{$value}px;";
-							},
-							'value'          => $this->attrs['heroMargin'],
-							'device_control' => false,
-						)
+					array(
+						'selector'       => ".gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_heroblock_wrapper",
+						'property'       => function ( $value ) {
+							return "margin: 0 0 -{$value}px -{$value}px;";
+						},
+						'value'          => $this->attrs['heroMargin'],
+						'device_control' => false,
 					)
 				);
 				$this->inject_style(
-					$this->inject_style(
-						array(
-							'selector'       => ".gvnews-block.gvnews-block-wrapper.{$this->element_id} article.gvnews_post",
-							'property'       => function ( $value ) {
-								return "padding: 0 0 {$value}px {$value}px;";
-							},
-							'value'          => $this->attrs['heroMargin'],
-							'device_control' => false,
-						)
+					array(
+						'selector'       => ".gvnews-block.gvnews-block-wrapper.{$this->element_id} article.gvnews_post",
+						'property'       => function ( $value ) {
+							return "padding: 0 0 {$value}px {$value}px;";
+						},
+						'value'          => $this->attrs['heroMargin'],
+						'device_control' => false,
 					)
 				);
 			}
