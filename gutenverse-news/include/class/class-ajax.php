e653c38380992a5caf641b4c2f3afc717337675a
tresna <tresnasaputra9@gmail.com>
2025-06-18 10:35:30 +0800

    [fix/module-header-filter] fix ajax handler when module name is not number

--- gutenverse-news/include/class/class-ajax.php
+++ gutenverse-news/include/class/class-ajax.php
@@ -112,7 +112,10 @@ class Ajax {
 				$module_prefix = $this->module_ajax_prefix;
 				if ( 0 === strpos( $action, $module_prefix ) ) {
 					$module_name  = str_replace( $module_prefix, '', $action );
-					$path         = sprintf( '%02d', str_replace( 'module_', '', $module_name ) );
+					$path         = str_replace( 'module_', '', $module_name );
+					if ( is_numeric($path) ) {
+						$path = sprintf( '%02d', $path);
+					}
 					$path         = "block-{$path}";
 					$module_file  = file_get_contents( GUTENVERSE_NEWS_DIR . 'block/' . str_replace( '_', '-', $path ) . '/block.json' );
 					$module_data  = json_decode( $module_file, true );
