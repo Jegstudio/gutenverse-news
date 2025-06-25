3bde44afd1e1d550c4229eb1f87275f0b1e3205e
Hakim <ragamgit78@gmail.com>
2025-06-25 13:26:11 +0800

    [fix/slider-gradient-overlay] fix overlay in slider

--- gutenverse-news/include/class/style/class-slider.php
+++ gutenverse-news/include/class/style/class-slider.php
@@ -43,6 +43,53 @@ class Slider extends StyleAbstract {
 	}
 
 	/**
+	 * Creating custom handle background to handle old values because format is changed
+	 *
+	 * @param string $selector .
+	 * @param object $background .
+	 */
+	public function custom_handle_gradient( $selector, $background ) {
+		$this->inject_style(
+			array(
+				'selector'       => $selector,
+				'property'       => function ( $value ) {
+					$gradient_color        = $value['gradientColor'];
+					$gradient_type         = $value['gradientType'];
+					$gradient_angle        = $value['gradientAngle'];
+					$gradient_radial       = $value['gradientRadial'];
+
+					if ( ! empty( $gradient_color ) ) {
+						$colors = array();
+
+						foreach ( $gradient_color as $gradient ) {
+							$offset  = $gradient['offset'] * 100;
+							$colors[] = "{$gradient['color']} {$offset}%";
+						}
+
+						$colors = join( ',', $colors );
+
+						if ( 'radial' === $gradient_type ) {
+							return "background: radial-gradient(at {$gradient_radial}, {$colors});";
+						} else {
+							return "background: linear-gradient({$gradient_angle}deg, {$colors});";
+						}
+					}
+				},
+				'value'          => array(
+					'gradientColor'       => isset( $background['gradientColor'] ) ? $background['gradientColor'] : null,
+					'gradientPosition'    => isset( $background['gradientPosition'] ) ? $background['gradientPosition'] : 0,
+					'gradientEndColor'    => isset( $background['gradientEndColor'] ) ? $background['gradientEndColor'] : null,
+					'gradientEndPosition' => isset( $background['gradientEndPosition'] ) ? $background['gradientEndPosition'] : 100,
+					'gradientType'        => isset( $background['gradientType'] ) ? $background['gradientType'] : 'linear',
+					'gradientAngle'       => isset( $background['gradientAngle'] ) ? $background['gradientAngle'] : 180,
+					'gradientRadial'      => isset( $background['gradientRadial'] ) ? $background['gradientRadial'] : 'center center',
+				),
+				'device_control' => false,
+			)
+		);
+	}
+
+	/**
 	 * Generate style base on attribute.
 	 */
 	public function generate() {
@@ -50,17 +97,27 @@ class Slider extends StyleAbstract {
 			if ( isset( $this->attrs['overrideOverlay'] ) && 'gradient' === $this->attrs['overlayOption'] ) {
 				$this->handle_background( ".{$this->element_id} .gvnews_overlay_slider_wrapper:before", $this->attrs['overrideOverlay'] );
 			}
-		} elseif ( stristr( $this->attrs['gvnewsModule'], 'Slider_2' ) ) {
-			if ( isset( $this->attrs['overrideOverlay'] ) && 'gradient' === $this->attrs['overlayOption'] ) {
-				$this->handle_background( ".{$this->element_id} .gvnews_slider_type_2 .gvnews_slide_item:before", $this->attrs['overrideOverlay'] );
+		}
+
+		if ( isset( $this->attrs['hasOverlay'] ) && $this->attrs['hasOverlay'] ) {
+
+			if ( isset( $this->attrs['overrideOverlay'] ) && $this->attrs['isOverrideOverlay'] && ( 'gradient' === $this->attrs['overlayOption'] ) ) {
+				$this->custom_handle_gradient(
+					".gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_slide_item:before",
+					$this->attrs['overrideOverlay']
+				);
 			}
-		} elseif ( stristr( $this->attrs['gvnewsModule'], 'Slider_5' ) ) {
-			if ( isset( $this->attrs['overrideOverlay'] ) && 'gradient' === $this->attrs['overlayOption'] ) {
-				$this->handle_background( ".{$this->element_id} .gvnews_slider_type_5_wrapper:not(.no-overlay) .gvnews_slider_type_5 .gvnews_slide_item:before", $this->attrs['overrideOverlay'] );
-			}
-		} elseif ( stristr( $this->attrs['gvnewsModule'], 'Slider_6' ) ) {
-			if ( isset( $this->attrs['overrideOverlay'] ) && 'gradient' === $this->attrs['overlayOption'] ) {
-				$this->handle_background( ".{$this->element_id} .gvnews_slider_type_6_wrapper:not(.no-overlay) .gvnews_slider_type_6 .gvnews_slide_item:before", $this->attrs['overrideOverlay'] );
+			if ( isset( $this->attrs['normalOverlay'] ) && ( 'normal' === $this->attrs['overlayOption'] ) ) {
+				$this->inject_style(
+					array(
+						'selector'       => ".gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_slide_item:before",
+						'property'       => function ( $value ) {
+							return $this->handle_color( $value, 'background-color' );
+						},
+						'value'          => $this->attrs['normalOverlay'],
+						'device_control' => false,
+					)
+				);
 			}
 		}
 	}
--- src/editor/blocks/slider-2/panels/panel-slider.js
+++ src/editor/blocks/slider-2/panels/panel-slider.js
@@ -1,13 +1,12 @@
 import { __ } from '@wordpress/i18n';
-import { SelectControl, CheckboxControl, RangeControl, TextControl, BackgroundControl } from 'gutenverse-core/controls';
-import { handleBackground } from 'gutenverse-core/styling';
+import { SelectControl, CheckboxControl, RangeControl, TextControl, GradientWithAngleControl, BackgroundControl, ColorControl } from 'gutenverse-core/controls';
 
 export const sliderPanel = (props) => {
     const {
-        elementId,
         autoplay,
         metaDateFormat,
-        overlayOption
+        overlayOption,
+        isOverrideOverlay,
     } = props;
     return [
         {
@@ -68,24 +67,31 @@ export const sliderPanel = (props) => {
                     value: 'gradient'
                 },
                 {
+                    label: __('Normal Overlay', 'gutenverse-news'),
+                    value: 'normal'
+                },
+                {
                     label: __('No Overlay', 'gutenverse-news'),
                     value: 'no'
                 },
             ],
         },
         {
+            id: 'isOverrideOverlay', // just for gradient type
+            show: overlayOption === 'gradient',
+            component: CheckboxControl,
+            label: __('Override Gradient Overlay')
+        },
+        {
             id: 'overrideOverlay',
-            show: overlayOption == 'gradient',
-            allowDeviceControl: true,
-            options: ['gradient'],
-            component: BackgroundControl,
-            style: [
-                {
-                    selector: `.gvnews-block.gvnews-block-wrapper.${elementId} .gvnews_slide_item:before`,
-                    hasChild: true,
-                    render: value => handleBackground(value)
-                }
-            ]
+            show: isOverrideOverlay && overlayOption === 'gradient',
+            component: GradientWithAngleControl,
+        },
+        {
+            id: 'normalOverlay',
+            label: __('Normal Overlay Color', 'gutenverse-news'),
+            show: overlayOption === 'normal',
+            component: ColorControl,
         },
     ];
 };
