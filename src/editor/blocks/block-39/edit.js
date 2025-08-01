7440202e975cf86342936aad4793d36dfa78ad34
Arie Cristian <ari@jegstudio.com>
2025-08-01 11:12:51 +0800

    [dev/trim-free-version] show deprecated block to pro user

--- src/editor/blocks/block-39/edit.js
+++ src/editor/blocks/block-39/edit.js
@@ -2,17 +2,24 @@ import { compose } from '@wordpress/compose';
 import { withPartialRender, withPassRef } from 'gutenverse-core/hoc';
 import Block39Columns from './Block39Columns';
 import DeprecatedBlockModule from '../../part/deprecated-module';
+import { gutenverseProActive } from '../../utils/helper';
+import BlockModule from '../../part/module';
+import { panelList } from './panels/panel-list';
 
 const Block39Block = compose(
     withPartialRender,
     withPassRef
 )((props) => {
     const columnAttr = {
-        block       : Block39Columns,
-        blockWidth  : 12,
+        block: Block39Columns,
+        blockWidth: 12,
     };
     const moduleName = '39';
-    return <DeprecatedBlockModule columnAttr={columnAttr} moduleName={moduleName} {...props} />;
+    if (gutenverseProActive) {
+        return <BlockModule columnAttr={columnAttr} moduleName={moduleName} {...props} panelList={panelList} />;
+    } else {
+        return <DeprecatedBlockModule columnAttr={columnAttr} moduleName={moduleName} {...props} />;
+    }
 });
 
 export default Block39Block;
