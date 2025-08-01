74b33447d9fe9222ccd0cbab62e5b47a3d6cf157
Arie Cristian <ari@jegstudio.com>
2025-08-01 13:34:34 +0800

    [dev/trim-free-version] lock header filter panel to pro user

--- src/editor/blocks/block-03/panels/panel-list.js
+++ src/editor/blocks/block-03/panels/panel-list.js
@@ -2,11 +2,12 @@ import { __ } from '@wordpress/i18n';
 import { advancePanel, backgroundPanel, borderPanel, responsivePanel } from 'gutenverse-core/controls';
 import { filterPanel } from '../../../control-panel/panel-filter';
 import { headerSettingsPanel, headerStylesPanel } from '../../../control-panel/panel-header';
-import { headerFilterPanelDeprecated } from '../../../control-panel/panel-hedaer-filter-deprecated';
+import { headerFilterPanel } from '../../../control-panel/panel-header-filter';
 import { settingPanel } from '../../../control-panel/panel-setting';
 import { paginationPanel } from '../../../control-panel/panel-pagination';
 import { designPanel } from '../../../control-panel/panel-design';
 import { TabSetting, TabStyle } from 'gutenverse-core/controls';
+import { gutenverseProActive } from '../../../utils/helper';
 
 export const panelList = () => {
     return [
@@ -22,9 +23,12 @@ export const panelList = () => {
             id: 'header-filter',
             title: __('Header Filter', 'gutenverse-news'),
             initialOpen: false,
-            panelArray: headerFilterPanelDeprecated,
+            panelArray: (props) => {
+                return headerFilterPanel(props);
+            },
             tabRole: TabSetting,
-            pro: true
+            deprecated: !gutenverseProActive,
+            pro: gutenverseProActive,
         },
         {
             title: __('Content Filter', 'gutenverse-news'),
