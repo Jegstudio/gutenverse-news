ea702a9d24421d5f9517c935d41537ee6a136b42
Arie Cristian <ari@jegstudio.com>
2025-08-12 14:54:50 +0800

    [dev/new-dashboard-settings] fix applyFilters when merging

--- src/editor/control-panel/panel-filter.js
+++ src/editor/control-panel/panel-filter.js
@@ -1,5 +1,4 @@
 import { __ } from '@wordpress/i18n';
-import { applyFilters } from '@wordpress/hooks';
 import { NumberControl, RangeControl, SelectSearchControl, SelectControl, CheckboxControl } from 'gutenverse-core/controls';
 import { searchPosts, searchPages, searchCategory, searchAuthor, searchTag } from '../utils/helper';
 import { applyFilters } from '@wordpress/hooks';
