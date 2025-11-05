a290bbbbaa64ecbbc1d1b658cd2ac19ff39ec272
Hakim <ragamgit78@gmail.com>
2025-11-05 17:58:57 +0800

    [dev/image-resize-feature] add image resize on module 5

--- src/editor/blocks/block-05/Block5Columns.js
+++ src/editor/blocks/block-05/Block5Columns.js
@@ -2,7 +2,20 @@ import ThumbModule from '../../part/thumbnail';
 import { ContentModule } from '../../part/post';
 
 const Block5Columns = props => {
-    const {postData, numberPost, paginationPost = numberPost, page, isLoadMore = false, moduleOption, excerptLength, excerptEllipsis, metaDateType, metaDateFormat, metaDateFormatCustom} = props;
+    const {
+        postData,
+        numberPost,
+        paginationPost = numberPost,
+        page,
+        isLoadMore = false,
+        moduleOption,
+        excerptLength,
+        excerptEllipsis,
+        metaDateType,
+        metaDateFormat,
+        metaDateFormatCustom,
+        renderedImageSizeMain = {},
+    } = props;
     const postDataLen = postData.length;
     const loadValidAnim = postDataLen - paginationPost;
 
@@ -10,7 +23,7 @@ const Block5Columns = props => {
         const { post, index = 'x' } = props;
         return (
             <article className={`gvnews_post gvnews_pl_lg_2 ${isLoadMore && index >= loadValidAnim && index <= postDataLen && page > 1 ? `gvnews_ajax_loaded anim_${(index - loadValidAnim)}` : ''}`}>
-                <ThumbModule size={715} cat={true} post={post}/>
+                <ThumbModule size={715} cat={true} post={post} imageSize={renderedImageSizeMain}/>
                 <ContentModule title={true} meta={1} excerpt={true} read={true} post={post} attr={props.attr}/>
             </article>
         );
