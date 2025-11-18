3545e3eda71309e292c981a4cca128139981f47c
Hakim <ragamgit78@gmail.com>
2025-11-18 11:41:11 +0800

    Revert "[dev/image-resize-feature] move thumb-overlay position into thumbnail-conatiner oon editor"
    This reverts commit 346dfa06faad2586607d5a657b261a569ce00b76.
    
    e

--- src/editor/part/thumbnail.js
+++ src/editor/part/thumbnail.js
@@ -6,24 +6,23 @@ const ThumbModule = (props) => {
         <a href="javascript:void(0)">
             <div className={`${props.classes}  thumbnail-container size-${imageSize.dimension ? imageSize.dimension : props.size} ${imageSize.class ? imageSize.class : ''}`}>
                 {props.post.thumbnail.url &&
-                    <>
-                        <img
-                            src={props.post.thumbnail.url}
-                            style={{
-                                objectFit: 'cover',
-                                verticalAlign: 'middle',
-                                maxHeight: '100%',
-                                maxWidth: '100%',
-                            }}
-                            height={imageSize.height}
-                            width={imageSize.width}
-                            className="lazyloaded"
-                        />
-                        <div className="gvnews-thumb-overlay"></div>
-                    </>
+                    <img
+                        src={props.post.thumbnail.url}
+                        style={{
+                            objectFit: 'cover',
+                            verticalAlign: 'middle',
+                            maxHeight: '100%',
+                            maxWidth: '100%',
+                        }}
+                        height={imageSize.height}
+                        width={imageSize.width}
+                        className="lazyloaded"
+                    />
                 }
             </div>
         </a>
+        <div className="gvnews-thumb-overlay">
+        </div>
         {props.cat && props.post.category.name && <MetaCategory {...props} />}
     </div>;
 };
