1913a2371e755148cecc0966503d35bcd8b85551
Hakim <ragamgit78@gmail.com>
2025-11-18 15:12:31 +0800

    [dev/image-resize-feature] fix selector on block-style for thumbnail-border

--- src/editor/control-panel/panel-styles/block-style.js
+++ src/editor/control-panel/panel-styles/block-style.js
@@ -1146,18 +1146,12 @@ const getBlockStyle = (
         isNotEmpty(attributes['borderMainThumbnail']) && data.push({
             'id': 'borderMainThumbnail',
             'type': 'border',
-            'selector': `
-                        .${elementId} .gvnews_postblock .${mainThumbnailClass} .thumbnail-container,
-                        .${elementId} .gvnews_postblock .${mainThumbnailClass} .gvnews-thumb-overlay
-                        `
+            'selector': `.${elementId} .gvnews_postblock .${mainThumbnailClass} .thumbnail-container`
         });
         isNotEmpty(attributes['borderResponsiveMainThumbnail']) && data.push({
             'id': 'borderResponsiveMainThumbnail',
             'type': 'borderResponsive',
-            'selector': `
-                        .${elementId} .gvnews_postblock .${mainThumbnailClass} .thumbnail-container,
-                        .${elementId} .gvnews_postblock .${mainThumbnailClass} .gvnews-thumb-overlay
-                        `
+            'selector': `.${elementId} .gvnews_postblock .${mainThumbnailClass} .thumbnail-container`
         });
         isNotEmpty(attributes['overlayBackgroundMain']) && data.push({
             'type': 'background',
@@ -1182,18 +1176,12 @@ const getBlockStyle = (
         isNotEmpty(attributes['borderSecondThumbnail']) && data.push({
             'id': 'borderSecondThumbnail',
             'type': 'border',
-            'selector': `
-                        .${elementId} .gvnews_postblock .${secondThumbnailClass} .thumbnail-container,
-                        .${elementId} .gvnews_postblock .${secondThumbnailClass} .gvnews-thumb-overlay
-                        `,
+            'selector': `.${elementId} .gvnews_postblock .${secondThumbnailClass} .thumbnail-container`,
         });
         isNotEmpty(attributes['borderResponsiveSecondThumbnail']) && data.push({
             'id': 'borderResponsiveSecondThumbnail',
             'type': 'borderResponsive',
-            'selector': `
-                        .${elementId} .gvnews_postblock .${secondThumbnailClass} .thumbnail-container,
-                        .${elementId} .gvnews_postblock .${secondThumbnailClass} .gvnews-thumb-overlay
-                        `,
+            'selector': `.${elementId} .gvnews_postblock .${secondThumbnailClass} .thumbnail-container`,
         });
         isNotEmpty(attributes['overlayBackgroundSecond']) && data.push({
             'type': 'background',
