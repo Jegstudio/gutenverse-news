43e4b908d67a18fb2dcc0268b432b1a1dbcb6bd8
Fauzan Edris <fauzan@jegstudio.com>
2025-12-10 18:00:50 +0800

    [dev/dequeue-font-library] fix: svg icon fallback

--- src/editor/part/header.js
+++ src/editor/part/header.js
@@ -79,10 +79,11 @@ function HeadTitle(props) {
     if ( !props.title && !props.second_title ) {
         return null;
     }
+    const finalIcon = (props.iconType === 'svg' && !props.iconSVG) ? '' : props.icon;
     return (
         <h3 className="gvnews_block_title">
             <span>
-                {props.icon && renderIcon(props.icon, props.iconType || 'icon', props.iconSVG || '')}
+                {finalIcon && renderIcon(finalIcon, props.iconType || 'icon', props.iconSVG || '')}
                 {props.title}
                 {props.second_title && <strong>
                     &nbsp;{props.second_title}
