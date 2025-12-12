77d9f343245a49612bec3133e4c526ce0187f317
Yoga <yoga@jegstudio.com>
2025-12-12 17:29:36 +0800

    [dev/dequeue-font-library] update slider icon to svg

--- src/editor/part/slider.js
+++ src/editor/part/slider.js
@@ -22,12 +22,12 @@ const SliderCaption = (props) => {
             </div>
             {props.navigation && <div className="gvnews_block_nav">
                 <a className="prev">
-                    <i className="fas fa-angle-left"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">{'<!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->'}<path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"></path></svg>
                     prev
                 </a>
                 <a className="next">
                     next
-                    <i className="fas fa-angle-right"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">{'<!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->'}<path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path></svg>                    <i className="fas fa-angle-right"></i>
                 </a>
             </div>}
         </div>
