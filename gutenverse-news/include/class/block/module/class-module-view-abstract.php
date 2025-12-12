af13faa7bbe57d361b1fdf60d89b941dc95c72fb
Yoga <yoga@jegstudio.com>
2025-12-12 17:19:48 +0800

    [dev/dequeue-font-library] update block pagination icon to svg

--- gutenverse-news/include/class/block/module/class-module-view-abstract.php
+++ gutenverse-news/include/class/block/module/class-module-view-abstract.php
@@ -164,13 +164,13 @@ abstract class Module_View_Abstract extends Block_View_Abstract {
 			$next = $next ? '' : 'disabled';
 			$prev = $prev ? '' : 'disabled';
 
-			$prev_text = '<i class="fas fa-chevron-left"></i>';
-			$next_text = '<i class="fas fa-chevron-right"></i>';
+			$prev_text = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"></path></svg>';
+			$next_text = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"></path></svg>';
 
 			if ( $attr['pagination_nextprev_showtext'] ) {
 				$additional_class .= ' showtext';
-				$prev_text         = '<i class="fas fa-chevron-left"></i> ' . esc_html__( 'Prev', 'gutenverse-news' );
-				$next_text         = esc_html__( 'Next', 'gutenverse-news' ) . '  <i class="fas fa-chevron-right"></i>';
+				$prev_text         = $prev_text . ' ' . esc_html__( 'Prev', 'gutenverse-news' );
+				$next_text         = esc_html__( 'Next', 'gutenverse-news' ) . ' ' . $next_text;
 			}
 
 			$output =
