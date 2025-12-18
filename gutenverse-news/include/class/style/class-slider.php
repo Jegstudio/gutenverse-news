e13193e7f3c441f23c450c3ef59ee1e0474afdd5
Hakim <ragamgit78@gmail.com>
2025-12-18 14:16:23 +0800

    [dev/slider-custom] fix hideStyleTitle on frontend

--- gutenverse-news/include/class/style/class-slider.php
+++ gutenverse-news/include/class/style/class-slider.php
@@ -398,7 +398,7 @@ class Slider extends StyleAbstract {
 			);
 		}
 
-		if ( isset( $this->attrs['hideTitleStyling'] ) ) { // For Slider 4.
+		if ( isset( $this->attrs['hideTitleStyling'] ) && $this->attrs['hideTitleStyling'] ) { // For Slider 4.
 			$this->inject_style(
 				array(
 					'selector'       => ".gvnews-slider-4.{$this->element_id} .gvnews_slider_type_4_wrapper.gvnews_slider_wrapper .gvnews_slider_type_4 .tns-slide-active .gvnews_slide_caption:before,
