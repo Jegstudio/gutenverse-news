995c45a8338643ded060ec0e0a6ad7d894a3b5dd
Arie Cristian <ari@jegstudio.com>
2025-12-19 18:03:06 +0800

    [dev/remove-postlist-margin] fix module 37 style and adding post item gap options

--- src/editor/blocks/block-37/panels/panel-list.js
+++ src/editor/blocks/block-37/panels/panel-list.js
@@ -16,6 +16,7 @@ import { thumbnailSettingPanel } from '../../../control-panel/panel-thumbnail-se
 import { thumbnailOverlayPanel } from '../../../control-panel/panel-thumbnail-overlay';
 import { contentContainerPanel } from '../../../control-panel/panel-content-container';
 import { noContentPanel } from '../../../control-panel/panel-no-content';
+import { postItemGridPanel } from '../../../control-panel/panel-post-item';
 
 export const panelList = () => {
     return applyFilters(
@@ -84,6 +85,12 @@ export const panelList = () => {
                 title: __('Design', 'gutenverse-news'),
                 initialOpen: false,
                 panelArray: designPanel,
+                tabRole: TabStyle
+            },
+            {
+                title: __('Post Item', 'gutenverse-news'),
+                initialOpen: false,
+                panelArray: postItemGridPanel,
                 tabRole: TabStyle
             },
             {
