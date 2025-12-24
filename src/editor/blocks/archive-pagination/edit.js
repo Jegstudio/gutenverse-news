c8728973dfca9d6a247dbb95d4e267a41eb8b854
Fauzan Edris <fauzan@jegstudio.com>
2025-12-24 14:48:43 +0800

    [dev/dequeue-font-library] fix: nav pagination next and prev not showing in text is hidden on navigation 1 and 2 in archive pagination

--- src/editor/blocks/archive-pagination/edit.js
+++ src/editor/blocks/archive-pagination/edit.js
@@ -71,13 +71,13 @@ const ArchivePagination = compose(
             <div className={`gvnews_navigation gvnews_pagination gvnews_col_3o3 gvnews_page${paginationMode} gvnews_align${paginationAlign} ${!paginationNavtext && 'no_navtext'} ${!paginationPageinfo && 'no_pageinfo'}`}>
                 {isCenterAlign && <span className="page_info">Page 1 of 3</span>}
                 <div className="nav-wrapper">
-                    {!isNav3 && <a className="nav-item page_nav prev" data-id="2" href="javascript:void(0);"><span className="navtext">{renderIcon('fas fa-angle-left', 'svg', btoa(prevIcon))}{paginationPrevText}</span></a>}
+                    {!isNav3 && <a className="nav-item page_nav prev" data-id="2" href="javascript:void(0);">{renderIcon('fas fa-angle-left', 'svg', btoa(prevIcon))}<span className="navtext">{paginationPrevText}</span></a>}
                     {paginationAlign === 'left' && <span className="page_info">Page 1 of 3</span>}
                     <span className="nav-item page_number active">1</span>
                     <span className="nav-item page_number dots">…</span>
                     <a className="nav-item page_number" data-id="2" href="javascript:void(0);">2</a>
                     <a className="nav-item page_number" data-id="3" href="javascript:void(0);">3</a>
-                    {!isNav3 && <a className="nav-item page_nav next" data-id="2" href="javascript:void(0);"><span className="navtext">{paginationNextText}{renderIcon('fas fa-angle-right', 'svg', btoa(nextIcon))}</span></a>}
+                    {!isNav3 && <a className="nav-item page_nav next" data-id="2" href="javascript:void(0);"><span className="navtext">{paginationNextText}</span>{renderIcon('fas fa-angle-right', 'svg', btoa(nextIcon))}</a>}
                 </div>
                 {isNav3 && !isCenterAlign && <div className="next-prev-button">
                     <a className="page_nav prev" data-id="2" href="javascript:void(0);">{renderIcon('fas fa-angle-left', 'svg', btoa(prevIcon))}<span className="navtext">{paginationPrevText}</span></a>
