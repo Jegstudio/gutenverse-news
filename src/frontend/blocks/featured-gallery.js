1e6e3b69eaf64c159526a0dcbe51064cdf5f4ca2
Hakim <ragamgit78@gmail.com>
2025-12-29 17:55:50 +0800

    [dev/integrate-with-metabox] add svg icon on featured-gallery.js

--- src/frontend/blocks/featured-gallery.js
+++ src/frontend/blocks/featured-gallery.js
@@ -32,12 +32,12 @@ class GutenverseFeaturedGallery {
 				if (info.nextButton) {
 					info.nextButton.setAttribute('aria-label', 'Next button');
 					u(info.nextButton).addClass('tns-next');
-					u(info.nextButton).html(`<i class="fas fa-chevron-right"></i>`);
+					u(info.nextButton).html('<div class="gutenverse-icon-svg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"></path></svg></div>');
 				}
 				if (info.prevButton) {
 					info.prevButton.setAttribute('aria-label', 'Previous button');
 					u(info.prevButton).addClass('tns-prev');
-					u(info.prevButton).html(`<i class="fas fa-chevron-left"></i>`);
+					u(info.prevButton).html('<div class="gutenverse-icon-svg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"></path></svg></div>');
 				}
 			},
 		};
