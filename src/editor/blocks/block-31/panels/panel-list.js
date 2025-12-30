fae295d3c82ec21c1f4f1e9958d3c7ae5250da94
Arie Cristian <ari@jegstudio.com>
2025-12-30 16:42:42 +0800

    [dev/remove-postlist-margin] add post item gap option on module 30 & 31

--- src/editor/blocks/block-31/panels/panel-list.js
+++ src/editor/blocks/block-31/panels/panel-list.js
@@ -12,6 +12,7 @@ import { paginationStylePanel } from '../../../control-panel/panel-pagination-st
 import { metaPanel } from '../../../control-panel/panel-meta';
 import { metaStylePanel } from '../../../control-panel/panel-meta-style';
 import { noContentPanel } from '../../../control-panel/panel-no-content';
+import { postItemPanel } from '../../../control-panel/panel-post-item';
 
 export const panelList = () =>
     applyFilters(
@@ -68,6 +69,12 @@ export const panelList = () =>
                 title: __('Design', 'gutenverse-news'),
                 initialOpen: false,
                 panelArray: designPanel,
+                tabRole: TabStyle
+            },
+            {
+                title: __('Post Item', 'gutenverse-news'),
+                initialOpen: false,
+                panelArray: postItemPanel,
                 tabRole: TabStyle
             },
             {
