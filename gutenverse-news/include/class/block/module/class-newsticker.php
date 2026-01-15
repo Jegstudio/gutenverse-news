3c2b02623ff7bac39e6958b7db52dc0fbec99480
Arie Cristian <ari@jegstudio.com>
2026-01-15 17:40:00 +0800

    [dev/add-aria-label] adding aria label on carousel and next ticker button

--- gutenverse-news/include/class/block/module/class-newsticker.php
+++ gutenverse-news/include/class/block/module/class-newsticker.php
@@ -99,11 +99,11 @@ class Newsticker extends Block_View_Abstract {
                     </div>
                 </div>
 				<div class=\"gvnews_news_ticker_control\">
-					<div class=\"gvnews_news_ticker_prev gvnews_news_ticker_arrow\">" . $prev_icon_html . "</div>
-					<span class=\"nav-separator\"></span>
-					<div class=\"gvnews_news_ticker_next gvnews_news_ticker_arrow\">" . $next_icon_html . "</div>
+					<div class=\"gvnews_news_ticker_prev gvnews_news_ticker_arrow\" aria-label=\"" . esc_html__( 'previous slide', 'gutenverse-news' ) . '">' . $prev_icon_html . '</div>
+					<span class="nav-separator"></span>
+					<div class="gvnews_news_ticker_next gvnews_news_ticker_arrow" aria-label="' . esc_html__( 'next slide', 'gutenverse-news' ) . '">' . $next_icon_html . '</div>
                 </div>
-            </div>";
+            </div>';
 
 		return $output;
 	}
