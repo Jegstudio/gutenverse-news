76e520751df16b4ec2a19a1ce31f96ddd228479d
Arie Cristian <ari@jegstudio.com>
2026-01-29 11:23:36 +0800

    [trunk] fix warning error on html tag option on archive page

--- gutenverse-news/include/class/block/class-archive.php
+++ gutenverse-news/include/class/block/class-archive.php
@@ -98,6 +98,7 @@ class Archive extends Grab {
 				'meta_author'  => isset( $this->attributes['showMetaAuthor'] ) ? $this->attributes['showMetaAuthor'] : true,
 				'meta_comment' => isset( $this->attributes['showMetaComment'] ) ? $this->attributes['showMetaComment'] : true,
 			),
+			'post_title_html_tag'   => isset( $this->attributes['postTitleHtmlTag'] ) ? $this->attributes['postTitleHtmlTag'] : 'h3',
 		);
 
 		$attr = $this->archive_title( $attr );
@@ -218,16 +219,16 @@ class Archive extends Grab {
 	 */
 	private function archive_block( $attr ) {
 		if ( 'gutenverse/news-archive-block' === $this->name ) {
-			$attr['block_type']         = $this->attributes['blockType'];
-			$attr['number_post']        = $this->attributes['numberPost'];
-			$attr['excerpt_length']     = $this->attributes['excerptLength'];
-			$attr['excerpt_ellipsis']   = $this->attributes['excerptEllipsis'];
-			$attr['date_format']        = $this->attributes['dateFormat'];
-			$attr['date_format_custom'] = $this->attributes['dateFormatCustom'];
-			$attr['first_page']         = $this->attributes['firstPage'];
-			$attr['column_width']       = $this->attributes['columnWidth'];
-			$attr['gutter_width']       = isset( $this->attributes['gutterWidth'] ) ? $this->attributes['gutterWidth'] : 30;
-			$attr['image_load']         = isset( $this->attributes['imageLoad'] ) ? $this->attributes['imageLoad'] : '';
+			$attr['block_type']          = $this->attributes['blockType'];
+			$attr['number_post']         = $this->attributes['numberPost'];
+			$attr['excerpt_length']      = $this->attributes['excerptLength'];
+			$attr['excerpt_ellipsis']    = $this->attributes['excerptEllipsis'];
+			$attr['date_format']         = $this->attributes['dateFormat'];
+			$attr['date_format_custom']  = $this->attributes['dateFormatCustom'];
+			$attr['first_page']          = $this->attributes['firstPage'];
+			$attr['column_width']        = $this->attributes['columnWidth'];
+			$attr['gutter_width']        = isset( $this->attributes['gutterWidth'] ) ? $this->attributes['gutterWidth'] : 30;
+			$attr['image_load']          = isset( $this->attributes['imageLoad'] ) ? $this->attributes['imageLoad'] : '';
 			$attr['post_title_html_tag'] = isset( $this->attributes['postTitleHtmlTag'] ) ? $this->attributes['postTitleHtmlTag'] : 'h3';
 		}
 		return $attr;
