90e0c9100a5e75037b7549167383f10518886be0
Arie Cristian <ari@jegstudio.com>
2026-01-30 10:33:08 +0800

    [dev/add-animation-panel] add animation and html tag option on RSS feeds block

--- src/editor/blocks/rss/panels/panel-general.js
+++ src/editor/blocks/rss/panels/panel-general.js
@@ -27,8 +27,40 @@ export const generalPanel = (props) => {
             component: ImageRadioControl,
             options: [
                 {
-                    image: <img src={`${imgDir}/content-3.png`}/>,
+                    image: <img src={`${imgDir}/content-3.png`} />,
                     value: '3'
+                },
+            ],
+        },
+        {
+            id: 'postTitleHtmlTag',
+            label: __('Post Title HTML Tag', 'gutenverse-news'),
+            description: __('Choose HTML tag for the post title.', 'gutenverse-news'),
+            component: SelectControl,
+            options: [
+                {
+                    label: __('H1', 'gutenverse-news'),
+                    value: 'h1'
+                },
+                {
+                    label: __('H2', 'gutenverse-news'),
+                    value: 'h2'
+                },
+                {
+                    label: __('H3', 'gutenverse-news'),
+                    value: 'h3'
+                },
+                {
+                    label: __('H4', 'gutenverse-news'),
+                    value: 'h4'
+                },
+                {
+                    label: __('H5', 'gutenverse-news'),
+                    value: 'h5'
+                },
+                {
+                    label: __('H6', 'gutenverse-news'),
+                    value: 'h6'
                 },
             ],
         },
