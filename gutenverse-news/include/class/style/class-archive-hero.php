1b9bef8eb6e3e18f76ac5998444e6facb854afce
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-02-04 16:20:58 +0800

    [dev/add-no-content-style] inject no content style in frontend

--- gutenverse-news/include/class/style/class-archive-hero.php
+++ gutenverse-news/include/class/style/class-archive-hero.php
@@ -218,6 +218,7 @@ class Archive_Hero extends StyleAbstract {
 				$this->generate_meta_style();
 		}
 		$this->category_label_style();
+		$this->no_content_style();
 	}
 
 	/**
@@ -614,6 +615,62 @@ class Archive_Hero extends StyleAbstract {
 				return '.gvnews_heroblock .gvnews_post:not(.gvnews_hero_item_1, .gvnews_hero_item_4 , .gvnews_hero_item_5)';
 			default:
 				return '.gvnews_heroblock .gvnews_post:not(.gvnews_hero_item_1)';
+		}
+	}
+
+	private function no_content_style() {
+		if ( isset( $this->attrs['noContentTypography'] ) ) {
+			$this->inject_typography(
+				array(
+					'selector'       => ".{$this->element_id} .gvnews_empty_module",
+					'property'       => function ( $value ) {},
+					'value'          => $this->attrs['noContentTypography'],
+					'device_control' => false,
+				)
+			);
+		}
+
+		if ( isset( $this->attrs['noContentColor'] ) ) {
+			$this->inject_style(
+				array(
+					'selector'       => ".{$this->element_id} .gvnews_empty_module",
+					'property'       => function ( $value ) {
+						return $this->handle_color( $value, 'color' );
+					},
+					'value'          => $this->attrs['noContentColor'],
+					'device_control' => false,
+				)
+			);
+		}
+
+		if ( isset( $this->attrs['noContentBackground'] ) ) {
+			$this->handle_background( ".{$this->element_id} .gvnews_empty_module", $this->attrs['noContentBackground'] );
+		}
+
+		if ( isset( $this->attrs['noContentBorder'] ) ) {
+			$this->inject_style(
+				array(
+					'selector'       => ".{$this->element_id} .gvnews_empty_module",
+					'property'       => function ( $value ) {
+						return $this->handle_border_responsive( $value );
+					},
+					'value'          => $this->attrs['noContentBorder'],
+					'device_control' => true,
+				)
+			);
+		}
+
+		if ( isset( $this->attrs['noContentPadding'] ) ) {
+			$this->inject_style(
+				array(
+					'selector'       => ".{$this->element_id} .gvnews_empty_module",
+					'property'       => function ( $value ) {
+						return $this->handle_dimension( $value, 'padding' );
+					},
+					'value'          => $this->attrs['noContentPadding'],
+					'device_control' => true,
+				)
+			);
 		}
 	}
 }
