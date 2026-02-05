e8e82d8dfc33e1848f1a4d6f7d1e73bdfc6afe65
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-02-05 11:13:46 +0800

    [dev/add-no-content-style] handle no content preview in all carousel

--- src/editor/blocks/carousel-2/edit.js
+++ src/editor/blocks/carousel-2/edit.js
@@ -65,7 +65,8 @@ const Carousel2Block = compose(
         showMeta = true,
         showMetaDate = true,
         renderedImageSizeMain,
-        postTitleHtmlTag = 'h3'
+        postTitleHtmlTag = 'h3',
+        gutenversePreviewBlock = '',
     } = attributes;
 
     const metaSettings = {
@@ -301,6 +302,10 @@ const Carousel2Block = compose(
         if (firstRender.current) {
             return;
         }
+        if (gutenversePreviewBlock === 'noContent') {
+            setBlock(<div className="gvnews_empty_module">{moduleOption.string && moduleOption.string.no_content}</div>);
+            return;
+        }
         resetblock();
     }, [
         excerptLength,
@@ -319,7 +324,8 @@ const Carousel2Block = compose(
         showMeta,
         showMetaDate,
         renderedImageSizeMain,
-        postTitleHtmlTag
+        postTitleHtmlTag,
+        gutenversePreviewBlock
     ]);
 
     useEffect(() => {
