b2a5acc4583dfa0e2f45da88c8feeb6f5d4b921c
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-02-05 17:05:45 +0800

    [dev/rss-block-style] enableExcerpt in rss design panel

--- src/editor/blocks/rss/panels/panel-list.js
+++ src/editor/blocks/rss/panels/panel-list.js
@@ -58,7 +58,10 @@ export const panelList = () => {
             {
                 title: __('Design', 'gutenverse-news'),
                 initialOpen: false,
-                panelArray: designPanel,
+                panelArray: (props) => designPanel({
+                    ...props,
+                    enableExcerpt: true
+                }),
                 tabRole: TabStyle
             },
             {
