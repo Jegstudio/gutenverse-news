a5c5e6f851faaa8c781027144aa6729b07f0dcca
Fauzan Edris <fauzan@jegstudio.com>
2026-02-20 17:42:39 +0800

    [dev/integrate-with-essential] add post autoload option to dashboard

--- src/backend/dashboard/settings/additional-features.js
+++ src/backend/dashboard/settings/additional-features.js
@@ -7,7 +7,7 @@ import { useState } from '@wordpress/element';
 
 const AdditionalFeatures = ({ settingValues, updateSettingValues, saving, saveData, setPopupActive, setInstallPopup }) => {
 
-    const [features, setFeatures] = useState(settingValues.features || [])
+    const [features, setFeatures] = useState(settingValues.features || []);
     const updateValue = (id, value) => {
         let newFeatures = [...features];
         if (!value) {
@@ -65,7 +65,12 @@ const AdditionalFeatures = ({ settingValues, updateSettingValues, saving, saveDa
             title: 'Like & Dislike Post',
             desc: 'Flexible and Design-Friendly Contact Form builder plugin for WordPress',
             icon: <IconPaywallSVG />,
-        },
+        }, {
+            id: 'post_autoload',
+            title: __('Post Autoload', 'gutenverse-news'),
+            desc: __('Automatically load the next post when the user reaches the end of the current post.', 'gutenverse-news'),
+            icon: <IconPaywallSVG />,
+        }
     ];
 
     return (
