4fcdea24be29001df0b317cfa7ba5deba2265330
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-02-27 11:37:31 +0800

    [dev/integrate-with-post-donation] apply filter for implementing donation panel in the server

--- src/editor/blocks/post-author/panels/panel-list.js
+++ src/editor/blocks/post-author/panels/panel-list.js
@@ -7,22 +7,26 @@ import { avatarStylePanel } from './panel-avatar-style';
 import { nameStylePanel } from './panel-name-style';
 import { iconStylePanel } from './panel-icon-style';
 import { generalPanel } from './panel-general';
+import { applyFilters } from '@wordpress/hooks';
 
 export const panelList = () => {
-    return [
+    return applyFilters('gvnews.post-author.panel.panelList', [
         {
+            id: 'general',
             title: __('General', 'gutenverse-news'),
             initialOpen: false,
             panelArray: generalPanel,
             tabRole: TabSetting
         },
         {
+            id: 'display',
             title: __('Display', 'gutenverse-news'),
             initialOpen: false,
             panelArray: responsivePanel,
             tabRole: TabSetting
         },
         {
+            id: 'spacing',
             title: __('Spacing', 'gutenverse-news'),
             initialOpen: false,
             panelArray: layoutPanel,
@@ -30,30 +34,35 @@ export const panelList = () => {
         },
         // Style
         {
+            id: 'nameStyle',
             title: __('Name Style', 'gutenverse'),
             initialOpen: false,
             panelArray: nameStylePanel,
             tabRole: TabStyle
         },
         {
+            id: 'biographyStyle',
             title: __('Biography Style', 'gutenverse'),
             initialOpen: false,
             panelArray: biographyStylePanel,
             tabRole: TabStyle
         },
         {
+            id: 'avatarStyle',
             title: __('Avatar Style', 'gutenverse'),
             initialOpen: false,
             panelArray: avatarStylePanel,
             tabRole: TabStyle
         },
         {
+            id: 'iconStyle',
             title: __('Social Icon Style', 'gutenverse'),
             initialOpen: false,
             panelArray: iconStylePanel,
             tabRole: TabStyle
         },
         {
+            id: 'background',
             title: __('Background', 'gutenverse-news'),
             initialOpen: false,
             panelArray: (props) => backgroundPanel({
@@ -65,16 +74,18 @@ export const panelList = () => {
             tabRole: TabStyle,
         },
         {
+            id: 'border',
             title: __('Border', 'gutenverse-news'),
             initialOpen: false,
             panelArray: borderPanel,
             tabRole: TabStyle
         },
         {
+            id: 'condition',
             title: __('Condition', 'gutenverse-news'),
             panelArray: conditionPanel,
             initialOpen: false,
             pro: true
         },
-    ];
+    ]);
 };
