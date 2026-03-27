7efb5a42091a4b74e13a61eeb0e2ba676635e946
Arie Cristian <ari@jegstudio.com>
2026-03-27 11:57:54 +0800

    [trunk] fix readmore typography overrided by content typography on block archive

--- gutenverse-news/include/class/style/class-archive-block.php
+++ gutenverse-news/include/class/style/class-archive-block.php
@@ -774,7 +774,7 @@ class Archive_Block extends StyleAbstract {
 		if ( isset( $this->attrs['contentTypography'] ) ) {
 			$this->inject_typography(
 				array(
-					'selector'       => ".gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_postblock .gvnews_post_excerpt p,.gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_postblock .gvnews_post_excerpt .gvnews_readmore",
+					'selector'       => ".gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_postblock .gvnews_post_excerpt p,.gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_post_excerpt .gvnews_readmore",
 					'property'       => function ( $value ) {},
 					'value'          => $this->attrs['contentTypography'],
 					'device_control' => false,
@@ -784,7 +784,7 @@ class Archive_Block extends StyleAbstract {
 		if ( isset( $this->attrs['readmoreButtonTypography'] ) ) {
 			$this->inject_typography(
 				array(
-					'selector' => ".gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_readmore",
+					'selector' => ".gvnews-block.gvnews-block-wrapper.{$this->element_id} .gvnews_postblock .gvnews_post_excerpt .gvnews_readmore",
 					'value'    => $this->attrs['readmoreButtonTypography'],
 				)
 			);
