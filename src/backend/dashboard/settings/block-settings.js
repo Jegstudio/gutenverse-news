ce7167edee6b78e3c6725241fb0a633ac4442fa0
Arie Cristian <ari@jegstudio.com>
2026-04-23 11:23:36 +0800

    [dev/integrate-with-essential] add recursive category query option

--- src/backend/dashboard/settings/block-settings.js
+++ src/backend/dashboard/settings/block-settings.js
@@ -1,26 +1,24 @@
 import { __ } from '@wordpress/i18n';
 import { ControlCheckbox, ControlSelect } from 'gutenverse-core/backend';
-import { applyFilters } from '@wordpress/hooks';
 
 const BlockSettings = ({ settingValues, updateSettingValues, saving, saveData }) => {
     /* option default value */
     const { block_settings = {}, features = [] } = settingValues;
     const {
         date_type = 'published',
+        recursive_category = false,
     } = block_settings;
 
     const updateValue = (id, value) => {
         updateSettingValues('block_settings', id, value);
     };
 
-    const additionalMenu = [];
-
     return <>
         <div className="template-tab-body" style={{ paddingTop: '10px' }}>
             <ControlSelect
                 id={'date_type'}
                 title={__('Post Date Type', '--gctd--')}
-                description={__('Pick time Unit', '--gctd--')}
+                description={__('Choose which post date type that you want to show for global post date meta.', '--gctd--')}
                 value={date_type}
                 updateValue={updateValue}
                 options={[
@@ -33,6 +31,13 @@ const BlockSettings = ({ settingValues, updateSettingValues, saving, saveData })
                         value: 'modified'
                     }
                 ]}
+            />
+            <ControlCheckbox
+                id={'recursive_category'}
+                title={__('Enable Recursive Category', '--gctd--')}
+                description={__('Enables recursive category queries when retrieving post lists based on categories in Gutenverse News blocks.', '--gctd--')}
+                value={recursive_category}
+                updateValue={updateValue}
             />
         </div>
         <div className="actions">
