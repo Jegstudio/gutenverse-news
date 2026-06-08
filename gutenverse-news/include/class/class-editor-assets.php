4b20abc34c5f5ef40b9e81e1f5b4d4f4b8b976ef
Yoga <yoga@jegstudio.com>
2026-06-08 21:37:16 +0800

    [dev/main] fix incorrect config name

--- gutenverse-news/include/class/class-editor-assets.php
+++ gutenverse-news/include/class/class-editor-assets.php
@@ -142,7 +142,7 @@ class Editor_Assets {
 	 * @param array $config Config.
 	 */
 	public function block_config( $config ) {
-		$config['gutenverseFormImgDir']              = GUTENVERSE_NEWS_URL . '/assets/img';
+		$config['gutenverseNewsImgDir']              = GUTENVERSE_NEWS_URL . '/assets/img';
 		$config['pluginVersions'][ GUTENVERSE_NEWS ] = array(
 			'name'           => GUTENVERSE_NEWS_NAME,
 			'version'        => GUTENVERSE_NEWS_VERSION,
