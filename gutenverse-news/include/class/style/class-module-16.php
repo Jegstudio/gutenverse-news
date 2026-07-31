d92c6d5a58098ee5e99d312c04eb55f304dfc25a
tresna <tresnasaputra9@gmail.com>
2026-07-31 16:11:02 +0800

    [dev/more-icon-options] seperate icon settings into its own handler

---
+++ gutenverse-news/include/class/style/class-module-16.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Module 16
+ *
+ * @author  Jegstudio
+ * @since   1.0.0
+ * @package gutenverse-news
+ */
+
+namespace GUTENVERSE\NEWS\Style;
+
+/**
+ * Module 16 additional Styling.
+ *
+ * @package gutenverse-news
+ */
+class Module_16 extends Block {
+    /**
+	 * Generate additional style.
+	 */
+	protected function generate_additional_style() {
+		if ( isset( $this->attrs['listIconSize'] ) ) {
+			$this->inject_style(
+				array(
+					'selector'       => "
+						.{$this->element_id} .gvnews_postblock_16 .gvnews_pl_xs_2 > .gutenverse-icon-svg:first-child svg,
+						.{$this->element_id} .gvnews_postblock_16 .gvnews_pl_xs_2 > i:first-child
+					",
+					'property'       => function ( $value ) {
+						return "font-size: {$value}px";
+					},
+					'value'          => $this->attrs['listIconSize'],
+					'device_control' => false,
+				)
+			);
+		}
+
+		if ( isset( $this->attrs['listIconSpacing'] ) ) {
+			$this->inject_style(
+				array(
+					'selector'       => ".{$this->element_id} .gvnews_postblock_16 .gvnews_pl_xs_2",
+					'property'       => function ( $value ) {
+						return "padding-left: {$value}px;";
+					},
+					'value'          => $this->attrs['listIconSpacing'],
+					'device_control' => false,
+				)
+			);
+		}
+
+		if ( isset( $this->attrs['listIconAlign'] ) ) {
+			$this->inject_style(
+				array(
+					'selector'       => "
+						.{$this->element_id} .gvnews_postblock_16 .gvnews_pl_xs_2 > .gutenverse-icon-svg:first-child svg,
+						.{$this->element_id} .gvnews_postblock_16 .gvnews_pl_xs_2 > i:first-child
+					",
+					'property'       => function ( $value ) {
+						if ( 'center' === $value ) {
+							return 'top: 50%; transform: translateY(-50%);';
+						}
+						if ( 'bottom' === $value ) {
+							return 'bottom: 0;';
+						}
+						return 'top: 0;';
+					},
+					'value'          => $this->attrs['listIconAlign'],
+					'device_control' => false,
+				)
+			);
+		}
+	}
+}
--- gutenverse-news/include/class/style/class-module-24.php
+++ gutenverse-news/include/class/style/class-module-24.php
@@ -15,7 +15,61 @@ namespace GUTENVERSE\NEWS\Style;
  * @package gutenverse-news
  */
 class Module_24 extends Block {
+	 /**
+	 * Generate additional style.
+	 */
+	protected function generate_additional_style() {
+		if ( isset( $this->attrs['listIconSize'] ) ) {
+			$this->inject_style(
+				array(
+					'selector'       => "
+						.{$this->element_id} .gvnews_postblock_24 .gvnews_pl_xs_4 .gvnews_postblock_content > .gutenverse-icon-svg:first-child svg,
+						.{$this->element_id} .gvnews_postblock_24 .gvnews_pl_xs_4 .gvnews_postblock_content > .i:first-child
+					",
+					'property'       => function ( $value ) {
+						return "font-size: {$value}px";
+					},
+					'value'          => $this->attrs['listIconSize'],
+					'device_control' => false,
+				)
+			);
+		}
 
+		if ( isset( $this->attrs['listIconSpacing'] ) ) {
+			$this->inject_style(
+				array(
+					'selector'       => ".{$this->element_id} .gvnews_postblock_24 .gvnews_pl_xs_4 .gvnews_postblock_content",
+					'property'       => function ( $value ) {
+						return "padding: 0 0 0 {$value}px;";
+					},
+					'value'          => $this->attrs['listIconSpacing'],
+					'device_control' => false,
+				)
+			);
+		}
+
+		if ( isset( $this->attrs['listIconAlign'] ) ) {
+			$this->inject_style(
+				array(
+					'selector'       => "
+						.{$this->element_id} .gvnews_postblock_24 .gvnews_pl_xs_4 .gvnews_postblock_content > .gutenverse-icon-svg:first-child svg,
+						.{$this->element_id} .gvnews_postblock_24 .gvnews_pl_xs_4 .gvnews_postblock_content > .i:first-child
+					",
+					'property'       => function ( $value ) {
+						if ( 'center' === $value ) {
+							return 'top: 50%; transform: translateY(-50%);';
+						}
+						if ( 'bottom' === $value ) {
+							return 'bottom: 0;';
+						}
+						return 'top: 0;';
+					},
+					'value'          => $this->attrs['listIconAlign'],
+					'device_control' => false,
+				)
+			);
+		}
+	}
 	/**
 	 * Generate style block post item style.
 	 */
